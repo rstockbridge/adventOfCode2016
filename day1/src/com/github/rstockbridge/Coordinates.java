bafccd201b2e06c9aac36dbcf3aec33e7f018a2d
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2016-12-03 22:45:01 -0500

    Add in solution for Part II
    Add function calcRepeatedLocationPartII(), and rename
    calcFinalLocation() to calFinalLocationPartII().

--- day1/src/com/github/rstockbridge/Coordinates.java
+++ day1/src/com/github/rstockbridge/Coordinates.java
@@ -4,11 +4,34 @@ class Coordinates {
     int x;
     int y;
 
+    public Coordinates(int inputX, int inputY) {
+        x = inputX;
+        y = inputY;
+    }
+
     void moveX(int distance) {
         x += distance;
     }
 
     void moveY(int distance) {
         y += distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordinates that = (Coordinates) o;
+
+        if (x != that.x) return false;
+        return y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
     }
 }
--- day1/src/com/github/rstockbridge/EasterBunnyHeadquarters.java
+++ day1/src/com/github/rstockbridge/EasterBunnyHeadquarters.java
@@ -1,5 +1,8 @@
 package com.github.rstockbridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.lang.Math.abs;
 
 public class EasterBunnyHeadquarters {
@@ -12,20 +15,25 @@ public class EasterBunnyHeadquarters {
             "L2, L1, R5, R2, L4, L1, R4, R3, L3, R1, R5, L1, L4, R2, L3, R5, R3, R1, L3";
 
     public static void main(String[] args) {
-        Coordinates location = new Coordinates();
-        calcFinalLocation(location);
-        displayNumBlocks(location.x, location.y);
+        Coordinates locationPartI = calcFinalLocationPartI();
+        displayNumBlocks(locationPartI.x, locationPartI.y);
+
+        Coordinates locationPartII = calcRepeatedLocationPartII();
+        if (locationPartII != null) {
+            displayNumBlocks(locationPartII.x, locationPartII.y);
+        } else {
+            System.out.println("No locations are visited twice.");
+        }
     }
 
-    private static void calcFinalLocation(Coordinates location) {
-        Orientation previous;
+    private static Coordinates calcFinalLocationPartI() {
+        Coordinates location = new Coordinates(0, 0);
+        Orientation previous = Orientation.NORTH;
         Orientation next;
         Turn turn;
         int distance;
 
         final String[] directions = ALL_DIRECTIONS.split(", ");
-
-        previous = Orientation.NORTH;
 
         for (String direction : directions) {
             turn = Turn.getTurn(direction.charAt(0));
@@ -54,10 +62,86 @@ public class EasterBunnyHeadquarters {
 
             previous = next;
         }
+        return location;
+    }
+
+    private static Coordinates calcRepeatedLocationPartII() {
+        Coordinates location = new Coordinates(0, 0);
+        Orientation previous = Orientation.NORTH;
+        Orientation next;
+        Turn turn;
+        int distance;
+
+        final String[] directions = ALL_DIRECTIONS.split(", ");
+
+        List<Coordinates> placesVisited = new ArrayList<Coordinates>();
+        placesVisited.add(new Coordinates(0, 0));
+
+        for (String direction : directions) {
+            turn = Turn.getTurn(direction.charAt(0));
+            distance = Integer.parseInt(direction.substring(1, direction.length()));
+
+            if (turn == Turn.RIGHT) {
+                next = previous.turnRight();
+            } else {
+                next = previous.turnLeft();
+            }
+
+            switch (next) {
+                case NORTH:
+                    for (int step = 1; step <= distance; step++) {
+                        Coordinates place = new Coordinates(location.x, location.y + step);
+                        if (placesVisited.contains(place)) {
+                            return place;
+                        } else {
+                            placesVisited.add(place);
+                        }
+                    }
+                    location.moveY(distance);
+                    break;
+                case SOUTH:
+                    for (int step = 1; step <= distance; step++) {
+                        Coordinates place = new Coordinates(location.x, location.y - step);
+                        if (placesVisited.contains(place)) {
+                            return place;
+                        } else {
+                            placesVisited.add(place);
+                        }
+                    }
+                    location.moveY(-distance);
+                    break;
+                case EAST:
+                    for (int step = 1; step <= distance; step++) {
+                        Coordinates place = new Coordinates(location.x + step, location.y);
+                        if (placesVisited.contains(new Coordinates(location.x + step, location.y))) {
+                            return place;
+                        } else {
+                            placesVisited.add(place);
+                        }
+                    }
+                    location.moveX(distance);
+                    break;
+                case WEST:
+                    for (int step = 1; step <= distance; step++) {
+                        Coordinates place = new Coordinates(location.x - step, location.y);
+                        if (placesVisited.contains(place)) {
+                            return place;
+                        } else {
+                            placesVisited.add(place);
+                        }
+                    }
+                    location.moveX(-distance);
+                    break;
+            }
+
+            previous = next;
+        }
+        return null;
     }
 
     private static void displayNumBlocks(int xCoordinate, int yCoordinate) {
-        System.out.println(abs(xCoordinate) + abs(yCoordinate));
+        System.out.format("Easter Bunny Headquarters is %d blocks away.\n", abs(xCoordinate) + abs(yCoordinate));
     }
+
 
 }
