531d080169ec05235e0ca8e342d9693769abfce6
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2016-12-11 23:28:09 -0500

    Add Part II of Day 5

--- day5/src/com/github/rstockbridge/MD5Password.java
+++ day5/src/com/github/rstockbridge/MD5Password.java
@@ -5,17 +5,21 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class MD5Password {
+
+    private static final char placeholderPasswordEntry = '&';
 
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
 
         final String doorID = "ffykfhsq";
-        System.out.format("The password is %s.\n", calculatePassword(doorID));
+        System.out.format("The Part I password is %s.\n", calculatePasswordPartI(doorID));
+        System.out.format("The Part II password is %s.\n", calculatePasswordPartII(doorID));
     }
 
 
-    private static String calculatePassword(String doorID) throws NoSuchAlgorithmException {
+    private static String calculatePasswordPartI(String doorID) throws NoSuchAlgorithmException {
         String combinedIDAndIndex;
         String password = "";
         int currentTestIndex = 0;
@@ -35,5 +39,36 @@ public class MD5Password {
         }
 
         return password;
+    }
+
+    private static String calculatePasswordPartII(String doorID) throws NoSuchAlgorithmException {
+        String combinedIDAndIndex;
+
+        char[] password = new char[8];
+        Arrays.fill(password, placeholderPasswordEntry);
+
+        int currentTestIndex = 0;
+        int successCount = 0;
+
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+
+        while (successCount < 8) {
+            combinedIDAndIndex = doorID + Integer.toString(currentTestIndex);
+            md5.update(StandardCharsets.UTF_8.encode(combinedIDAndIndex));
+            String hash = String.format("%032x", new BigInteger(1, md5.digest()));
+
+            if (hash.startsWith("00000")) {
+                int entryPositionCandidate = Character.getNumericValue(hash.charAt(5));
+
+                if ((entryPositionCandidate >= 0) && (entryPositionCandidate < 8) && (password[entryPositionCandidate] == placeholderPasswordEntry)) {
+                    password[entryPositionCandidate] = hash.charAt(6);
+                    successCount++;
+                }
+            }
+
+            currentTestIndex++;
+        }
+
+        return new String(password);
     }
 }
